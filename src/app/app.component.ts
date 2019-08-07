dcf505adbb2e7f26e4d6ec45ae603c0ee1188bb0
Peter Dolan <chinup.peter@gmail.com>
2019-08-07 13:09:48 -0500

    working on login

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,6 +4,7 @@ import { RouterExtensions } from "nativescript-angular/router";
 import { DrawerTransitionBase, RadSideDrawer, SlideInOnTopTransition } from "nativescript-ui-sidedrawer";
 import { filter } from "rxjs/operators";
 import * as app from "tns-core-modules/application";
+import dotenv from 'dotenv'
 
 @Component({
     moduleId: module.id,
@@ -19,6 +20,7 @@ export class AppComponent implements OnInit {
     }
 
     ngOnInit(): void {
+        dotenv.config();
         this._activatedUrl = "/home";
         this._sideDrawerTransition = new SlideInOnTopTransition();
 
