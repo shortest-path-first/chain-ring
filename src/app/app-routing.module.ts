be99efcb34a9b2851a466478e2e21bda8796db06
Peter Dolan <chinup.peter@gmail.com>
2019-08-06 19:34:19 -0500

    rerouted login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,7 +6,7 @@ const routes: Routes = [
     { path: "", redirectTo: "/home", pathMatch: "full" },
     { path: "home", loadChildren: "~/app/home/home.module#HomeModule" },
     { path: "browse", loadChildren: "~/app/browse/browse.module#BrowseModule" },
-    { path: "search", loadChildren: "~/app/search/search.module#SearchModule" },
+    { path: "login", loadChildren: "~/app/login/login.module#LoginModule" },
     { path: "map", loadChildren: "~/app/map/map.module#MapModule" },
     { path: "settings", loadChildren: "~/app/settings/settings.module#SettingsModule" }
 ];
