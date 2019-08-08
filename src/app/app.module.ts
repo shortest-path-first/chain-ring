8517d066c13d175947c9d24fee615238e718ff63
franco3445 <francoappss@gmail.com>
2019-08-08 15:41:55 -0500

    Search function working

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule, NO_ERRORS_SCHEMA } from "@angular/core";
 import { NativeScriptModule } from "nativescript-angular/nativescript.module";
 import { NativeScriptUISideDrawerModule } from "nativescript-ui-sidedrawer/angular";
 
+
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 
--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -2,6 +2,12 @@ import { Component, OnInit, ElementRef, ViewChild } from "@angular/core";
 import { RadSideDrawer } from "nativescript-ui-sidedrawer";
 import { registerElement } from "nativescript-angular/element-registry";
 import * as app from "tns-core-modules/application";
+import { HttpClient, HttpParams} from "@angular/common/http"
+import { Place } from "./map";
+import { Observable } from 'rxjs';
+
+
+
 
 
 registerElement("MapView", () => require("nativescript-google-maps-sdk").MapView);
@@ -14,18 +20,52 @@ registerElement("MapView", () => require("nativescript-google-maps-sdk").MapView
 })
 export class MapComponent implements OnInit {
 
-    constructor() {
-        // Use the component constructor to inject providers.
+    latitude = 30
+    longitude = -90.15
+    zoom = 11
 
+    readonly ROOT_URL = "https://7be0faec.ngrok.io"
+
+    places: Observable<Place[]>;
+
+    public searchBoxReturn(text) {
+
+        console.log(text);
+
+    }
+    constructor(private http: HttpClient) {
+        // Use the component constructor to inject providers.
     }
 
     ngOnInit(): void {
         // Init your component properties here.
+
     }
 
     onDrawerButtonTap(): void {
         const sideDrawer = <RadSideDrawer>app.getRootView();
         sideDrawer.showDrawer();
+    }
+
+    getPlaces(text) {
+
+        let params = new HttpParams().set('place', text);
+
+        console.log(text)
+
+        this.http.get<Place[]>(this.ROOT_URL + "/franco", {params}).subscribe(response => {
+            //do something with response
+            console.log(response);
+        }, err => {
+            console.log(err.message);
+        }, () => {
+            console.log("completed");
+        })
+
+
+
+
+
     }
 
     
