df324f400411348733a383e79a59b1f101c6b8f4
Peter Dolan <chinup.peter@gmail.com>
2019-08-08 22:03:56 -0500

    added test route listening

--- src/app/ride/ride.component.ts
+++ src/app/ride/ride.component.ts
@@ -4,6 +4,8 @@ import { registerElement } from "nativescript-angular/element-registry";
 import * as app from "tns-core-modules/application";
 import * as geolocation from "nativescript-geolocation";
 import { Accuracy } from "tns-core-modules/ui/enums"; 
+import { Color } from "tns-core-modules/color/color";
+var accelerometer = require("nativescript-accelerometer");
 
 var mapsModule = require("nativescript-google-maps-sdk");
 const decodePolyline = require('decode-google-map-polyline');
@@ -35,8 +37,7 @@ export class RideComponent implements OnInit {
     }
 
     onPinTap(): void {
-        console.log("Pin drop!")
-       
+        
         geolocation.getCurrentLocation({ desiredAccuracy: Accuracy.high, maximumAge: 5000, timeout: 20000 })
             .then((result) => {
                 let marker = new mapsModule.Marker();
@@ -44,10 +45,11 @@ export class RideComponent implements OnInit {
                 mapView.addMarker(marker);
             });
     }
-
+ 
     onMapReady(args){
         mapView = args.object;
-        let line = "yd}uDhjsdPaBH@l@t@lZ|@r]ZpMDjDFl@IfAIRiB`AsElBaEpB{Ax@gAf@oB`AcBz@sFjCgLtF{G~CiB~@gAh@[TaBn@eAh@{BbAaHdD_EnBsAj@iCr@uAlAi@XgFdCaHhDiFdCaGrC}BfAcEjBmEvB{OnHs@^LlEl@jUFrB@`BExCOpBc@hCYhAa@dAgGjNIPUTOLa@PUDMHQ^?d@@F@F?j@CTCTMToDdIhE`C|@Xh@^p@Xx@L\\@h@Eb@U~@S"
+        let newPath = new mapsModule.Polyline();
+        let line = "a`~uDhyxdPr@TxAaC~CeFhD}FtDcGdGyJbA_Bl@s@b@u@~@aB|DoGbJiOBEPW?ALQYWsBcBiEsD}HaHUW_@y@E_@KyFKyFQiHQeGSaJGkBPeAZw@p@qAV}@OaG[iMAQgBF"
         var flightPlanCoordinates = decodePolyline(line);
         
        const polyline = new mapsModule.Polyline();
@@ -55,15 +57,44 @@ export class RideComponent implements OnInit {
            let coord = flightPlanCoordinates[i];
            polyline.addPoint(mapsModule.Position.positionFromLatLng(coord.lat, coord.lng));
        }
-    
-
         polyline.visible = true;
         polyline.width = 10;
         polyline.geodesic = false;
+        polyline.color = new Color("purple");
         mapView.latitude = flightPlanCoordinates[0].lat;
         mapView.longitude = flightPlanCoordinates[0].lng;        
         mapView.zoom = 20;
+        mapView.tilt = 45;
         mapView.addPolyline(polyline);
-        
+        geolocation.getCurrentLocation({ desiredAccuracy: Accuracy.high, maximumAge: 5000, timeout: 20000 })
+            .then((result) => {
+                let marker = new mapsModule.Marker();
+                marker.position = mapsModule.Position.positionFromLatLng(result.latitude, result.longitude);
+                mapView.addMarker(marker);
+            });
+        // accelerometer.startAccelerometerUpdates(function (data) {
+        //     //  console.log("x: " + data.x + "y: " + data.y + "z: " + data.z);
+            
+        // }, { sensorDelay: "normal" });
+        geolocation.watchLocation((loc) => {
+            if (loc) {
+                console.log(loc.direction);
+                newPath.addPoint(mapsModule.Position.positionFromLatLng(loc.longitude, loc.latitude));
+                newPath.visible = true;
+                newPath.width = 10;
+                newPath.geodesic = false;
+                newPath.color = new Color("red")
+                mapView.addPolyline(newPath)
+                mapView.latitiude = loc.latitude;
+                mapView.longitude = loc.longitude;
+            }
+        }, (e) => {
+            console.log("Error: " + e.message);
+        }, {
+                desiredAccuracy: Accuracy.high,
+                updateDistance: 0.1,
+                updateTime: 3000,
+                minimumUpdateTime: 100
+            });
     }
 }
