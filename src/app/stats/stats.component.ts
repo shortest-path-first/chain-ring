f8c8d9d936bcaf3cf8a18040e4c7beda5ffa00d9
franco3445 <francoappss@gmail.com>
2019-08-10 14:51:22 -0500

    built stats page, need to get db call

--- src/app/stats/stats.component.ts
+++ src/app/stats/stats.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from "@angular/core";
 import { RadSideDrawer } from "nativescript-ui-sidedrawer";
 import * as app from "tns-core-modules/application";
+import { HttpClient, HttpParams } from "@angular/common/http";
+import { storedStats } from "./stats";
+import { Observable } from "rxjs";
 
 @Component({
     selector: "Stats",
@@ -9,10 +12,18 @@ import * as app from "tns-core-modules/application";
 })
 export class StatsComponent implements OnInit {
 
-    constructor() {
-        // Use the component constructor to inject providers.
-    }
+    averageSpeed = 22;
+    totalDistance = 1000;
+    moneySaved = 90000;
+    pieChart = 5;
+    readonly ROOT_URL = "https://2c7dbe02.ngrok.io";
 
+    storedStats: Observable<Array<storedStats>>;
+
+    constructor(private http: HttpClient) {
+        // Use the component constructor to inject providers.
+        this.placeHolder();
+    }
     ngOnInit(): void {
         // Init your component properties here.
     }
@@ -20,5 +31,14 @@ export class StatsComponent implements OnInit {
     onDrawerButtonTap(): void {
         const sideDrawer = <RadSideDrawer>app.getRootView();
         sideDrawer.showDrawer();
+    }
+
+    placeHolder() {
+        console.log("yaaaaaaaaaaaaas");
+    }
+
+    userTotalInfo() {
+        console.log("thats a more a ");
+        
     }
 }
