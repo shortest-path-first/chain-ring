f2a1cf7f03310917419521b5207e6363c72798d1
semaj <semajbarthelemy55@gmail.com>
2019-08-21 10:55:44 -0500

    return promise from tnsOauthLogin

--- src/auth-service.ts
+++ src/auth-service.ts
@@ -48,73 +48,91 @@ export function tnsOauthLogin(providerType) {
   const file: File = folder.getFile(`${vm.get("token") || "token"}` + `.txt`);
 
   client = new TnsOAuthClient(providerType);
-  client.loginWithCompletion((tokenResult: ITnsOAuthTokenResult, error) => {
-    if (error) {
-      console.error("error loggin in:", error);
-      // (error);
-    } else {
-      console.log("Logged in", tokenResult.idTokenExpiration);
-      console.log("Logged in", tokenResult.accessTokenExpiration);
-      console.log("Logged in", tokenResult.refreshTokenExpiration);
 
-      const options = {
-          url: "http://812bec1b.ngrok.io/userInfo",
-          method: "POST",
-          headers: { "Content-Type": "application/json" },
-          content: JSON.stringify({
-              accesstoken: tokenResult.accessToken,
-              idToken: tokenResult.idToken
-          })
-      };
-      request(options)
-        .then((result) => {
+  return new Promise((resolve, reject) => {
+    client.loginWithCompletion(
+        (tokenResult: ITnsOAuthTokenResult, error) => {
+            if (error !== undefined) {
+                console.error("error loggin in:", error);
+                // (error);
+            } else {
+                console.log("Logged in", tokenResult.idTokenExpiration);
+                console.log("Logged in", tokenResult.accessTokenExpiration);
+                console.log(
+                    "Logged in",
+                    tokenResult.refreshTokenExpiration
+                );
 
-          console.log(result.content.toString().slice(result.content.toString().indexOf(":") + 1)
-            .match(/[A-Z, 0-9]/gi)
-            .join(""));
-          // result.content.toFile("file:///src/token.txt");
-          file.writeText(
-              result.content
-                  .toString()
-                  .slice(result.content.toString().indexOf(":") + 1)
-                  .match(/[A-Z, 0-9]/gi)
-                  .join(""))
-              .then(() => {
-                  file.readText().then((res) => {
-                      vm.set(
-                          "successMessage",
-                          `Successfully saved in${file.path}`
-                      );
-                      vm.set("writtenContent", res);
-                      vm.set("isItemVisible", true);
+                const options = {
+                    url: "http://812bec1b.ngrok.io/userInfo",
+                    method: "POST",
+                    headers: { "Content-Type": "application/json" },
+                    content: JSON.stringify({
+                        accesstoken: tokenResult.accessToken,
+                        idToken: tokenResult.idToken
+                    })
+                };
+                request(options)
+                    .then(result => {
+                        console.log(
+                            result.content
+                                .toString()
+                                .slice(
+                                    result.content.toString().indexOf(":") +
+                                        1
+                                )
+                                .match(/[A-Z, 0-9]/gi)
+                                .join("")
+                        );
+                        // result.content.toFile("file:///src/token.txt");
+                        file.writeText(
+                            result.content
+                                .toString()
+                                .slice(
+                                    result.content.toString().indexOf(":") +
+                                        1
+                                )
+                                .match(/[A-Z, 0-9]/gi)
+                                .join("")
+                        )
+                            .then(() => {
+                                file.readText().then(res => {
+                                    vm.set(
+                                        "successMessage",
+                                        `Successfully saved in${file.path}`
+                                    );
+                                    vm.set("writtenContent", res);
+                                    vm.set("isItemVisible", true);
 
-                      request({
-                          url: `http://812bec1b.ngrok.io/login/${res}`,
-                          method: "Patch",
-                          headers: {
-                              "Content-Type":
-                                  "application/json"
-                          },
-                          content: JSON.stringify({
-                              token: res
-                          })
-                      }).then(response => {
-                          console.log(
-                              response.content.toString()
-                          );
-                      });
-                  });
-              })
-              .catch((err) => {
-                  console.log(err);
-              });
-        })
-        .catch((err) => {
-          console.error("err", err.stack);
-        });
+                                    request({
+                                        url: `http://812bec1b.ngrok.io/login/${res || "noexist"}`,
+                                        method: "Patch",
+                                        headers: {
+                                            "Content-Type":
+                                                "application/json"
+                                        },
+                                        content: JSON.stringify({
+                                            token: res
+                                        })
+                                    }).then(response => {
+                                        console.log(
+                                            response.content.toString()
+                                        );
+                                        resolve(response.content.toString());
+                                    });
+                                });
+                            })
+                            .catch(err => {
+                                console.log(err);
+                            });
+                    })
+                    .catch(err => {
+                        console.error("err", err.stack);
+                    });
+            }
 
-      }
-
-    // return client;
+            // return client;
+        }
+    );
   });
 }
