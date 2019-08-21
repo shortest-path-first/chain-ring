b53c3414ee57faa47cc1efd33fcf70f4bde14cf7
franco3445 <francoappss@gmail.com>
2019-08-21 17:36:17 -0500

    Merging to add location

--- src/app/locationAdd/locationAdd.ts
+++
@@ -1,9 +0,0 @@
-export interface Place {
-  formatted_address: string;
-  polyLine: string;
-  name: string;
-  opening_hours: boolean;
-  vicinity: string;
-  geometry: object;
-  location: object;
-}
